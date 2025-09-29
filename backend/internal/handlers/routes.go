55a382c521e18c0ef6213efd8142d4f520f1df73
Yousef Amirghofran <yamirghofran@gmail.com>
2025-09-29 12:59:55 +0200

    feat: add PodcastService to routes and create endpoints

--- backend/internal/handlers/routes.go
+++ backend/internal/handlers/routes.go
@@ -5,7 +5,7 @@ import (
 	"github.com/yamirghofran/briefbot/internal/services"
 )
 
-func SetupRoutes(router *gin.Engine, userService services.UserService, itemService services.ItemService, dailyDigestService services.DailyDigestService) {
-	handler := NewHandler(userService, itemService, dailyDigestService)
+func SetupRoutes(router *gin.Engine, userService services.UserService, itemService services.ItemService, dailyDigestService services.DailyDigestService, podcastService services.PodcastService) {
+	handler := NewHandler(userService, itemService, dailyDigestService, podcastService)
 	handler.SetupRoutes(router)
 }
